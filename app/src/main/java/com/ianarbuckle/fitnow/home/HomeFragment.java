7934da27f5000688b15d2d95660b58e7fee3c034
Ian Arbuckle <ian.arbuckle18@gmail.com>
2016-12-18 16:52:47 +0000

    FN-107
    - UI implementation for prototype

--- app/src/main/java/com/ianarbuckle/fitnow/home/HomeFragment.java
+++ app/src/main/java/com/ianarbuckle/fitnow/home/HomeFragment.java
@@ -6,14 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.ianarbuckle.fitnow.BaseFragment;
 import com.ianarbuckle.fitnow.BlankActivity;
-import com.ianarbuckle.fitnow.FitNowApplication;
 import com.ianarbuckle.fitnow.R;
-import com.ianarbuckle.fitnow.authentication.AuthPagerActivity;
-import com.ianarbuckle.fitnow.authentication.AuthPresenterImpl;
 import com.ianarbuckle.fitnow.walking.StartWalkPagerActivity;
 
 import butterknife.OnClick;
@@ -24,14 +19,6 @@ import butterknife.OnClick;
  */
 
 public class HomeFragment extends BaseFragment {
-
-  FirebaseUser firebaseUser;
-  FirebaseAuth firebaseAuth;
-
-  AuthPresenterImpl presenter;
-
-  String userName;
-  String photoUrl;
 
   public static HomeFragment newInstance() {
     return new HomeFragment();
@@ -44,26 +31,7 @@ public class HomeFragment extends BaseFragment {
   }
 
   @Override
-  public void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    firebaseAuth = FirebaseAuth.getInstance();
-    firebaseUser = firebaseAuth.getCurrentUser();
-    if(firebaseUser == null) {
-      startActivity(AuthPagerActivity.newIntent(getContext()));
-      getActivity().finish();
-      return;
-    } else {
-      userName = firebaseUser.getDisplayName();
-      if(firebaseUser.getPhotoUrl() != null) {
-        photoUrl = firebaseUser.getPhotoUrl().toString();
-      }
-    }
-
-  }
-
-  @Override
   protected void initPresenter() {
-    presenter = new AuthPresenterImpl(FitNowApplication.getAppInstance().getAuthenticationHelper());
   }
 
   @OnClick(R.id.runBtn)
